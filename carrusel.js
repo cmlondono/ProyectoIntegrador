16fce239cb4ec3d01349c01be11b14c9f014df8f
Brahian-Perez <perezvelezbrahiandavid@gmail.com>
2025-10-30 00:32:45 -0500

    se crearon los cambios de toda la vista derecha, la cual presenta las tarjetas en forma de carrusel de imagenes

---
+++ carrusel.js
@@ -0,0 +1,46 @@
+    let carrusel = document.querySelector(".carrusel-targeta");
+
+    carrusel.innerHTML = `
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  
+  <!-- Indicadores -->
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+  </div>
+
+  <!-- Contenedor de imágenes -->
+  <div class="carousel-inner d-flex gap-2">
+    <div class="carousel-item active">
+      <img src="Imagenes/tarjeta.png" class="d-block w-100" alt="..." style="height:100%; object-fit:cover;">
+    </div>
+    <div class="carousel-item">
+      <img src="Imagenes/tarjeta credito.png" class="d-block w-100" alt="..." style="height:100%; object-fit:cover;">
+    </div>
+    <div class="carousel-item">
+      <img src="Imagenes/CardsHome.PNG" class="d-block w-100" alt="..." style="height:100%; object-fit:cover;">
+    </div>
+    <div class="carousel-item">
+      <img src="Imagenes/tarjeta-exito-basica_1.png" class="d-block w-100" alt="..." style="height:100%; object-fit:cover;">
+    </div>
+    <div class="carousel-item">
+      <img src="Imagenes/Tarjeta-coral.PNG" class="d-block w-100" alt="..." style="height:100%; object-fit:cover;">
+    </div>
+  </div>
+
+  <!-- Controles de navegación (fuera del .carousel-inner) -->
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+
+</div>
+`;
