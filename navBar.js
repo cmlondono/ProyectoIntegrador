951cead84eedcc81eb82a9205b7ca77cee42ee8f
Brahian-Perez <perezvelezbrahiandavid@gmail.com>
2025-11-01 11:29:04 -0500

    se crearon las demas opciones como, perfil, nueva tarjeta etc

--- navBar.js
+++ navBar.js
@@ -1,37 +1,41 @@
 let navBar = document.querySelector(".divmenu");
 
-navBar.innerHTML=`
+navBar.innerHTML = `
       <nav class="navmenu " style="background-color: rgba(53, 71, 172, 255)">
         <div class="logo-usuario">
           <img src="Imagenes/imagenRostroPrueba.jpg" alt="" id="imagenRostroPrueba" style="text-align: center" />
         </div>
 
-        <h2 style="text-align: center; color: white" class="">
+        <h2 style="text-align: center; color: white" class="nombre-usuario">
           Nombre ejemplo
         </h2>
         <div class="container-opciones-perfil-medio">
+
           <div class="opcion">
-            <img src="iconos/dashBoardIcono.png" alt="" class="iconoNav" />
-            <span>Dashboard</span>
+            <a style="text-decoration: none; color: white" href="perfil.html">
+            <img src="iconos/perfileIcono.png" alt="" class="iconoNav" /></a>
+            <a style="text-decoration: none; color: white" href="perfil.html"><span>Perfil</span></a>
+
           </div>
+
           <div class="opcion">
-            <img src="iconos/perfileIcono.png" alt="" class="iconoNav" />
-            <span>Perfil</span>
-          </div>
-          <div class="opcion">
-            <a style="text-decoration: none; color: white" href="analicis.html"><img src="iconos/analiticaIcono.png"
-                alt="" class="iconoNav" /></a>
+            <a style="text-decoration: none; color: white" href="analicis.html">
+            <img src="iconos/analiticaIcono.png" alt="" class="iconoNav" /></a>
             <a style="text-decoration: none; color: white" href="analicis.html"><span>Estadisticas</span></a>
           </div>
+
           <div class="opcion">
-            <img src="iconos/notasIcono.png" alt="" class="iconoNav" />
-            <span>Notas</span>
+            <a style="text-decoration: none; color: white" href="notas.html"><img src="iconos/notasIcono.png" 
+            alt="" class="iconoNav" /></a>
+            <span>Notas</span></a>
           </div>
         </div>
 
         <div class="container-opciones-perfil-inferior">
           <div class="opcion">
-            <span><img src="iconos/ajustesIcono.png" alt="" class="iconoNav" />Ajustes</span>
+            <a style="text-decoration: none; color: white" href="ajustes.html"><img src="iconos/ajustesIcono.png" 
+            alt="" class="iconoNav" /></a>
+             <a style="text-decoration: none; color: white" href="ajustes.html"><span>Ajustes</span></a>
           </div>
           <div class="opcion">
             <img src="iconos/salidaIcono.png" alt="" class="iconoNav" />
